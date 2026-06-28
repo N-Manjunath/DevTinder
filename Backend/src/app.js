fdb582092455bb6908751275c2bd61916724f5be
N-Manjunath <manjunathmanju200727@gmail.com>
2026-06-28 12:06:03 +0530

    allowed origin

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://devtinder-app-alpha.vercel.app"
+    "https://dev-tinder-six-nu.vercel.app"
   ],
   credentials: true
 }));
